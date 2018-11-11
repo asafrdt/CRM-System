2d78a4780d2331d46a056a600246fdfc4cb22f1d
Asafrdt <asafrdt@gmail.com>
2018-11-11 15:11:46 +0200

    אסף

--- Team5 project/Team5 project/main.Designer.cs
+++ Team5 project/Team5 project/main.Designer.cs
@@ -29,26 +29,72 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(560, 174);
+            this.button1.Location = new System.Drawing.Point(180, 39);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(231, 130);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "איזור אישי";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(423, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(220, 129);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "קופה";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(423, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(220, 130);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "חנות";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(180, 186);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(231, 129);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "דוחות";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(74, 39);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Logout";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(900, 562);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Main";
-            this.Text = "Form2";
+            this.Text = "CEO MAIN PAGE";
             this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
 
@@ -57,5 +103,9 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
