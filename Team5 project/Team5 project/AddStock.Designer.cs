1928a3fec9bef24833244397f9a08f9fc44bb2ef
tair <tairhadad1@gmail.com>
2018-12-24 00:33:44 +0200

    comboBox1 DataTable

--- Team5 project/Team5 project/AddStock.Designer.cs
+++ Team5 project/Team5 project/AddStock.Designer.cs
@@ -38,7 +38,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
@@ -46,7 +46,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(284, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(176, 30);
+            this.label1.Size = new System.Drawing.Size(143, 23);
             this.label1.TabIndex = 0;
             this.label1.Text = "Serial number";
             // 
@@ -55,7 +55,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(284, 52);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(183, 30);
+            this.label2.Size = new System.Drawing.Size(151, 23);
             this.label2.TabIndex = 1;
             this.label2.Text = "Product name";
             // 
@@ -64,7 +64,7 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(284, 95);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(115, 30);
+            this.label3.Size = new System.Drawing.Size(93, 23);
             this.label3.TabIndex = 2;
             this.label3.Text = "Quantity";
             // 
@@ -72,21 +72,21 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(474, 6);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 37);
+            this.textBox1.Size = new System.Drawing.Size(100, 32);
             this.textBox1.TabIndex = 5;
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(473, 49);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 37);
+            this.textBox2.Size = new System.Drawing.Size(100, 32);
             this.textBox2.TabIndex = 6;
             // 
             // textBox3
             // 
             this.textBox3.Location = new System.Drawing.Point(473, 92);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 37);
+            this.textBox3.Size = new System.Drawing.Size(100, 32);
             this.textBox3.TabIndex = 7;
             // 
             // button1
@@ -108,7 +108,7 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(284, 135);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(72, 30);
+            this.label4.Size = new System.Drawing.Size(57, 23);
             this.label4.TabIndex = 3;
             this.label4.Text = "Price";
             // 
@@ -117,7 +117,7 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(284, 180);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(110, 30);
+            this.label5.Size = new System.Drawing.Size(90, 23);
             this.label5.TabIndex = 4;
             this.label5.Text = "Provider";
             // 
@@ -125,23 +125,24 @@
             // 
             this.textBox4.Location = new System.Drawing.Point(473, 135);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 37);
+            this.textBox4.Size = new System.Drawing.Size(100, 32);
             this.textBox4.TabIndex = 8;
             // 
-            // textBox5
+            // comboBox1
             // 
-            this.textBox5.Location = new System.Drawing.Point(473, 177);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 37);
-            this.textBox5.TabIndex = 9;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(473, 177);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 31);
+            this.comboBox1.TabIndex = 11;
             // 
             // AddStock
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -175,6 +176,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- Team5 project/Team5 project/AddStock.cs
+++ Team5 project/Team5 project/AddStock.cs
@@ -17,8 +17,28 @@ namespace Team5_project
         public AddStock()
         {
             InitializeComponent();
+            Fillcombo();
         }
-        
+        void Fillcombo()
+        {
+            SqlCommand cmd;
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\PROJECT\TEAM5\TEAM5\TEAM5 PROJECT\DATABASE\STOREMANGE.MDF;Integrated Security=True;Connect Timeout=30");
+            SqlDataAdapter sda = new SqlDataAdapter("select * from Suppllier ", conn);
+            comboBox1.Items.Clear();
+            conn.Open();
+            cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select Full_name from Suppllier";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            sda = new SqlDataAdapter(cmd);
+            sda.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                comboBox1.Items.Add(dr["Full_name"].ToString());
+            }
+            conn.Close();
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\PROJECT\TEAM5\TEAM5\TEAM5 PROJECT\DATABASE\STOREMANGE.MDF;Integrated Security=True;Connect Timeout=30");
@@ -31,7 +51,7 @@ namespace Team5_project
             }
             else
             {
-                SqlCommand sda2 = new SqlCommand("INSERT INTO Inventory (Serialnumber,Product_name,Quantity,Price,Provider) VALUES ('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" + textBox5.Text + "')", conn);
+                SqlCommand sda2 = new SqlCommand("INSERT INTO Inventory (Serialnumber,Product_name,Quantity,Price,Provider) VALUES ('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" + comboBox1.Text + "')", conn);
                 SqlDataAdapter da = new SqlDataAdapter(sda2);
                 DataSet ds = new DataSet();
                 da.Fill(ds);
